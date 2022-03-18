0f51db0df5e86fa296e6bfe26d30688dc3a2b07c
aigarsmen <aigars.mendiks@vidusskolalielvarde.lv>
2022-03-18 11:47:49 +0200

    qwqe

---
+++ script.js
@@ -0,0 +1,17 @@
+//console.log(2)
+let zina = document.querySelector('.manaZina');//.value//piekļūst laukumam, value uzzina tieši kas rakstits
+let zinas = document.querySelector('.chataZinas');
+function sutitZinu()
+{
+    console.log('sutit');
+
+    zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value; //mainam html
+}
+
+async function ieladetChataZinas()
+{
+    let datiNoServera = await fetch('chatazinas.txt');
+    let dati = await datiNoServera.text(); //pasaka grib redzet kā tekstu
+
+    zinas.innerHTML = dati;
+}
